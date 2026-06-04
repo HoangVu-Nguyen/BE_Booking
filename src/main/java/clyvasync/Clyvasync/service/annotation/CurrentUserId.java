d1f398bb2bd5895844ededdd3c40041bad5b0a45
HoangVu-Nguyen <hoangvufit2004@gmail.com>
2026-06-04 14:27:42 +0700

    fix: sửa biểu thức SpEL tại CurrentUserId thành claims để tương thích với đối tượng Jwt mặc định trên Production

--- src/main/java/clyvasync/Clyvasync/service/annotation/CurrentUserId.java
+++ src/main/java/clyvasync/Clyvasync/service/annotation/CurrentUserId.java
@@ -9,6 +9,6 @@ import java.lang.annotation.Target;
 @Target(ElementType.PARAMETER)
 @Retention(RetentionPolicy.RUNTIME)
 
-@AuthenticationPrincipal(expression = "#this.token.claims['user_id']")
+@AuthenticationPrincipal(expression = "claims['user_id']")
 public @interface CurrentUserId {
 }
