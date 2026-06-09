31cbcbf35863622a1c7b00004563e3d1c966da83
HoangVu-Nguyen <hoangvufit2004@gmail.com>
2026-06-09 15:09:57 +0700

    feature(s3): add  logic file

--- src/main/java/clyvasync/Clyvasync/config/S3Config.java
+++ src/main/java/clyvasync/Clyvasync/config/S3Config.java
@@ -7,26 +7,34 @@ import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
-
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 @Configuration
 public class S3Config {
-
-    @Value("${aws.access-key}")
-    private String accessKey;
-
-    @Value("${aws.secret-key}")
-    private String secretKey;
-
-    @Value("${aws.region}")
-    private String region;
+    @Value("${aws.s3.access-key}")
+    private  String AWS_ACCESS_KEY;
+    @Value("${aws.s3.secret-key}")
+    private  String AWS_SECRET_KEY;
+    @Value("${aws.s3.region}")
+    private  String REGION;
+    @Bean
+    public S3Presigner s3Presigner() {
+        return S3Presigner.builder()
+                .region(Region.of(REGION))
+                .credentialsProvider(StaticCredentialsProvider.create(
+                        AwsBasicCredentials.create(AWS_ACCESS_KEY, AWS_SECRET_KEY)))
+                .build();
+    }
+    @Bean
+    public StaticCredentialsProvider awsCredentialsProvider() {
+        AwsBasicCredentials credentials = AwsBasicCredentials.create(AWS_ACCESS_KEY, AWS_SECRET_KEY);
+        return StaticCredentialsProvider.create(credentials);
+    }
 
     @Bean
     public S3Client s3Client() {
-        AwsBasicCredentials credentials = AwsBasicCredentials.create(accessKey, secretKey);
-
         return S3Client.builder()
-                .region(Region.of(region))
-                .credentialsProvider(StaticCredentialsProvider.create(credentials))
+                .region(Region.of(REGION))
+                .credentialsProvider(awsCredentialsProvider())
                 .build();
     }
 }
---
+++ src/main/java/clyvasync/Clyvasync/service/media/S3Service.java
@@ -0,0 +1,10 @@
+package clyvasync.Clyvasync.service.media;
+
+import java.util.List;
+
+public interface S3Service {
+    String generatePresignedPutUrl(String objectKey, String contentType, Long fileSize);
+    void deleteFile(String objectKey);
+    String getPublicUrl(String objectKey) ;
+    void deleteFiles(List<String> objectKeys);
+}
