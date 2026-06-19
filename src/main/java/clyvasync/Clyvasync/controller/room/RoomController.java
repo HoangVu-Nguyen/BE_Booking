5954dc3c6e36f95281fac48c5d443b244bb81bff
HoangVu-Nguyen <hoangvufit2004@gmail.com>
2026-06-19 18:47:17 +0700

    feat(room): implement api room

--- src/main/java/clyvasync/Clyvasync/controller/room/RoomController.java
+++ src/main/java/clyvasync/Clyvasync/controller/room/RoomController.java
@@ -1,14 +1,12 @@
 package clyvasync.Clyvasync.controller.room;
 
 
-import clyvasync.Clyvasync.dto.request.BatchUploadRequest;
-import clyvasync.Clyvasync.dto.request.BookingInitRequest;
-import clyvasync.Clyvasync.dto.request.MultiRoomBatchUploadRequest;
-import clyvasync.Clyvasync.dto.request.RoomBatchUpdateRequest;
+import clyvasync.Clyvasync.dto.request.*;
 import clyvasync.Clyvasync.dto.response.*;
 import clyvasync.Clyvasync.service.annotation.CurrentUserId;
 import clyvasync.Clyvasync.service.booking.BookingService;
 import clyvasync.Clyvasync.service.homestay.HomestayRoomService;
+import clyvasync.Clyvasync.service.room.RatePlanBenefitMappingService;
 import clyvasync.Clyvasync.service.room.RoomCalendarService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
@@ -23,6 +21,7 @@ import java.util.List;
 public class RoomController {
     private final RoomCalendarService roomCalendarService;
     private final HomestayRoomService homestayRoomService;
+    private final RatePlanBenefitMappingService ratePlanBenefitService;
 
     @GetMapping("/homestays/{homestayId}/unavailable-dates")
     public ApiResponse<List<LocalDate>> getUnavailableDates(
@@ -54,6 +53,41 @@ public class RoomController {
         System.out.println(request);
 
         homestayRoomService.updateRooms(ownerId, request);
+
+        return ApiResponse.success();
+    }
+    @GetMapping("/{roomId}/rate-plans/{ratePlanId}/benefits")
+    public ApiResponse<List<RatePlanBenefitResponse>> getRatePlanBenefits(
+            @CurrentUserId Long ownerId,
+            @PathVariable Long homestayId,
+            @PathVariable Long roomId,
+            @PathVariable Long ratePlanId
+    ) {
+        return ApiResponse.success(
+                ratePlanBenefitService.getRatePlanBenefits(
+                        ownerId,
+                        homestayId,
+                        roomId,
+                        ratePlanId
+                )
+        );
+    }
+
+    @PutMapping("/{roomId}/rate-plans/{ratePlanId}/benefits")
+    public ApiResponse<Void> updateRatePlanBenefits(
+            @CurrentUserId Long ownerId,
+            @PathVariable Long homestayId,
+            @PathVariable Long roomId,
+            @PathVariable Long ratePlanId,
+            @RequestBody UpdateRatePlanBenefitsRequest request
+    ) {
+        ratePlanBenefitService.updateRatePlanBenefits(
+                ownerId,
+                homestayId,
+                roomId,
+                ratePlanId,
+                request
+        );
 
         return ApiResponse.success();
     }
