419f33daeb1bbc6b6dade603d4be55d3de3fadd1
HoangVu-Nguyen <hoangvufit2004@gmail.com>
2026-06-27 14:39:55 +0700

    feat: add HostKycProfileResponse and mapper

--- src/main/java/clyvasync/Clyvasync/dto/response/HostKycProfileResponse.java
+++ src/main/java/clyvasync/Clyvasync/dto/response/HostKycProfileResponse.java
@@ -1,4 +1,18 @@
 package clyvasync.Clyvasync.dto.response;
 
+import clyvasync.Clyvasync.enums.kyc.KycProfileStatus;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class HostKycProfileResponse {
+    private String legalName;
+    private String idCardNumber;
+    private String idCardIssuedBy;
+    private String bankName;
+    private String bankAccountNumber;
+    private String bankAccountOwner;
+    private KycProfileStatus status;
+    private String rejectionReason;
 }
--- src/main/java/clyvasync/Clyvasync/mapper/kyc/HostKycMapper.java
+++ src/main/java/clyvasync/Clyvasync/mapper/kyc/HostKycMapper.java
@@ -1,4 +1,13 @@
 package clyvasync.Clyvasync.mapper.kyc;
 
+import clyvasync.Clyvasync.dto.response.HostKycProfileResponse;
+import clyvasync.Clyvasync.modules.kyc.entity.HostKycProfile;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
 public interface HostKycMapper {
+    HostKycProfileResponse toHostKycProfileResponse(HostKycProfile hostKycProfile);
+    List<HostKycProfileResponse> toHostKycProfileResponseList(List<HostKycProfile> hostKycProfiles);
 }
